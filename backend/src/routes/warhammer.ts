a1ba4e4fede0187567b9169a7d7f3c2dcb838b06
swoerve <swoerve@gmail.com>
2025-12-04 16:04:37 +0100

    split route into route and controller for warhammer.ts

--- backend/src/routes/warhammer.ts
+++ backend/src/routes/warhammer.ts
@@ -1,70 +1,12 @@
 import express from 'express'
-import axios from 'axios'
-import * as xml2js from 'xml2js'
-import { decode } from 'js-base64'
-
+import { getWarhammerCatData } from '../controllers/warhammer.ts'
 
 const cat = express.Router()
 
-const parser = new xml2js.Parser()
 
-cat.get('/', (req, res) => {
-  axios.get('https://api.github.com/repos/BSData/wh40k-10e/contents/Necrons.cat').then((response) => {
-    // console.log(response)
-    let result = decodeAndFix(response.data.content)
-    res.json(result)
-  })
+cat.get('/', async (req, res) => {
+  let result = await getWarhammerCatData()
+  res.json(result)
 })
 
-function decodeAndFix(input: string): any {
-  let decoded = decode(input)
-  let parsed: Record<string, any> | null = null
-  parser.parseString(decoded, (err, result) => {
-    if (err) {
-      throw "uh oh"
-    }
-    parsed = result
-  })
-
-  if (parsed == null) {
-    return { error: 'something did not work right' }
-  }
-  let fixed = recursiveJsonFix(parsed)
-  return fixed
-}
-
 export default cat
-
-function recursiveJsonFix(obj: Record<string, any>): Record<string, any> {
-  let fixedObj: Record<string, any> = {}
-  for (let property in obj) {
-    if (property !== '$') {
-      if (typeof obj[property] == "object" && !Array.isArray(obj[property])) {
-        fixedObj[property] = recursiveJsonFix(obj[property])
-      } else if (Array.isArray(obj[property])) {
-        if (obj[property].length <= 1) {
-          fixedObj[property] = recursiveJsonFix(obj[property][0])
-        } else {
-          let fixedArr = []
-
-          for (let item in obj[property]) {
-            let fixedItem = recursiveJsonFix(obj[property][item])
-            fixedArr.push(fixedItem)
-          }
-          fixedObj[property] = fixedArr
-        }
-      }
-
-    }
-  }
-  if (obj.hasOwnProperty("$")) {
-    for (let property in obj["$"]) {
-      fixedObj[property] = obj["$"][property]
-    }
-  }
-  if (obj.hasOwnProperty("_")) {
-    fixedObj['value'] = obj['_']
-  }
-
-  return fixedObj
-}
