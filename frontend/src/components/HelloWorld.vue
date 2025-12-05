7ee9946111578013b0f69f4c682a5590772c8f3d
Kåre Lindqvist <karelindqvist@gmail.com>
2025-12-05 19:46:05 +0100

    add tailwind, reformat frontend files

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,41 +1,28 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref } from "vue";
 
-defineProps<{ msg: string }>()
+defineProps<{ msg: string }>();
 
-const count = ref(0)
+const count = ref(0);
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <div
+    class="flex flex-col gap-2 p-8 sm:flex-row sm:items-center sm:gap-6 sm:py-4 ..."
+  >
+    <div class="space-y-2 text-center sm:text-left">
+      <div class="space-y-0.5">
+        <p class="font-medium text-gray-500">Below is a count button</p>
+      </div>
+      <button
+        type="button"
+        @click="count++"
+        class="border-purple-200 text-purple-600 hover:bg-red-600 hover:text-black active:bg-purple-700 sm:flex-row sm:items-center sm:gap-6 sm:py-4"
+      >
+        count is {{ count }}
+      </button>
+    </div>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
+<style scoped></style>
