64b51ab917ac2e41761d0447d4f08c955d6784f3
swoerve <swoerve@gmail.com>
2025-12-11 13:47:29 +0100

    added revision 2 of database, kept revision 1 commented

--- database/db.sql
+++ database/db.sql
@@ -1,4 +1,42 @@
+-- rev 1
+-- CREATE TABLE IF NOT EXISTS accounts {
+--   id integer PRIMARY KEY,
+--   username varchar NOT NULL,
+--   pass varchar NOT NULL
+-- }
+--
+-- CREATE TABLE IF NOT EXISTS armies {
+--   id integer PRIMARY KEY,
+--   owner_id integer REFERENCES accounts (id),
+--   name varchar,
+--   max_points integer,
+--   created_at timestamp
+-- }
+--
+-- CREATE TABLE IF NOT EXISTS units {
+--   id varchar PRIMARY KEY,
+--   army_id integer REFERENCES armies (id),
+--   name varchar NOT NULL,
+--   xml_id varchar NOT NULL,
+-- }
+--
+-- CREATE TABLE IF NOT EXISTS selection_groups {
+--   id integer PRIMARY KEY,
+--   unit_id integer REFERENCES units (id),
+--   ref_id integer,
+--   name varchar NOT NULL,
+--   xml_id varchar NOT NULL
+-- }
+--
+-- CREATE TABLE IF NOT EXISTS selection_entries {
+--   id integer PRIMARY KEY,
+--   group_id integer REFERENCES selection_groups (id),
+--   name varchar NOT NULL,
+--   checked bool NOT NULL,
+--   xml_id varchar NOT NULL
+-- }
 
+-- rev 2
 CREATE TABLE IF NOT EXISTS accounts {
   id integer PRIMARY KEY,
   username varchar NOT NULL,
@@ -7,7 +45,7 @@ CREATE TABLE IF NOT EXISTS accounts {
 
 CREATE TABLE IF NOT EXISTS armies {
   id integer PRIMARY KEY,
-  owner_id integer REFERENCES accounts (id),
+  owner_id integer REFERENCES accounts (id) ON DELETE CASCADE,
   name varchar,
   max_points integer,
   created_at timestamp
@@ -15,23 +53,8 @@ CREATE TABLE IF NOT EXISTS armies {
 
 CREATE TABLE IF NOT EXISTS units {
   id varchar PRIMARY KEY,
-  army_id integer REFERENCES armies (id),
+  army_id integer REFERENCES armies (id) ON DELETE CASCADE,
   name varchar NOT NULL,
   xml_id varchar NOT NULL,
-}
-
-CREATE TABLE IF NOT EXISTS selection_groups {
-  id integer PRIMARY KEY,
-  unit_id integer REFERENCES units (id),
-  ref_id integer,
-  name varchar NOT NULL,
-  xml_id varchar NOT NULL
-}
-
-CREATE TABLE IF NOT EXISTS selection_entries {
-  id integer PRIMARY KEY,
-  group_id integer REFERENCES selection_groups (id),
-  name varchar NOT NULL,
-  checked bool NOT NULL,
-  xml_id varchar NOT NULL
+  selection json NOT NULL
 }
