58992c9be45a7bc2e6cb8faeb2762edaa4cd0619
swoerve <swoerve@gmail.com>
2025-12-15 19:39:43 +0100

    start work on uniteditor

---
+++ frontend/src/components/UnitEditor.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import { selectionEntry, selectionGroup } from "../utils/interfaces.ts";
+
+type Unit = {
+  id: number;
+  name: string;
+  type: string;
+  points: number;
+};
+
+const props = defineProps<{
+  unitData: Unit;
+}>();
+</script>
+
+<template>
+  <section
+    id="uniteditor"
+    class="bg-gray-100 rounded-lg shadow-md p-6 flex flex-col h-full"
+  >
+    <h3
+      id="unitname"
+      class="text-xl font-semibold mb-4 shrink-0"
+      v-if="props.unitData"
+    >
+      {{ props.unitData.name }}
+    </h3>
+  </section>
+</template>
+
+<style scoped>
+.scrollbar-thin::-webkit-scrollbar {
+  width: 6px;
+}
+.scrollbar-thin::-webkit-scrollbar-track {
+  background: #e5e7eb;
+  border-radius: 4px;
+}
+.scrollbar-thin::-webkit-scrollbar-thumb {
+  background-color: #9ca3af;
+  border-radius: 4px;
+}
+</style>
