4d354a1459233be5ac6c05bebd10fb4186bef7a8
swoerve <swoerve@gmail.com>
2025-12-16 23:20:48 +0100

    initial Modal component

---
+++ frontend/src/components/Modal.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="modal-mask">
+    <div class="modal-wrapper">
+      <div id="modal" class="modal-container"></div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
