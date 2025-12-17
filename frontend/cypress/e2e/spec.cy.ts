d42e2a34c3c9025c216dfdc20536f58d3dc16df6
swoerve <swoerve@gmail.com>
2025-12-17 20:01:47 +0100

    simple tests for login, signup pages

--- frontend/cypress/e2e/spec.cy.ts
+++ frontend/cypress/e2e/spec.cy.ts
@@ -1,5 +1,50 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("WH40K Builder", () => {
+  beforeEach(() => {
+    // cy.origin("http://localhost:5173", () => {
+    cy.visit("http://localhost:5173/");
+    // });
+  });
+
+  it("User can see the welcome page", () => {
+    cy.contains("h1", "WH40K ARMY BUILDER");
+  });
+
+  it("User can travel to login page then signup", () => {
+    cy.get("button").should("exist");
+    cy.get("button").click();
+
+    cy.url().should("eq", "http://localhost:5173/login");
+
+    cy.get("#register-button").should("exist");
+    cy.get("#register-button").click();
+
+    cy.url().should("eq", "http://localhost:5173/register");
+  });
+
+  it("User can login", () => {
+    cy.intercept("POST", "/api/account/login", {
+      statusCode: 200,
+      body: {
+        id: 3,
+        username: "test1",
+        email: "test1",
+      },
+    });
+    cy.intercept("GET", "/api/army/*", {
+      statusCode: 200,
+      body: [],
+    });
+    cy.get("button").should("exist");
+    cy.get("button").click();
+
+    cy.url().should("eq", "http://localhost:5173/login");
+
+    cy.get("#username").type("test1");
+    cy.get("#password").type("test1");
+
+    cy.get("button").click();
+
+    cy.url().should("eq", "http://localhost:5173/army-roster");
+  });
+});
+
