8345e72f908b6f67a0d3de28b2a54739e701afdf
swoerve <swoerve@gmail.com>
2025-12-17 01:44:15 +0100

    added sessionStorage for account id

--- frontend/src/stores/globalState.ts
+++ frontend/src/stores/globalState.ts
@@ -9,9 +9,19 @@ const accountState: { value: accountState } = { value: {} };
 // sets the accountState to the newState value
 export function setAccount(newState: accountState) {
   accountState.value = newState;
+  if (newState.id) {
+    sessionStorage.setItem("account", "" + newState.id);
+  }
 }
 
 // gets the accountState and provides its whole value
 export function getAccount() {
   return accountState.value;
+}
+
+export function checkSessionAccount() {
+  const sessionId = sessionStorage.getItem("account");
+  if (sessionId) {
+    accountState.value.id = Number(sessionId);
+  }
 }
